e704e8eed5f8fa7fe3ce1539b01d6a3c7d33c68f
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-05-16 13:05:30 +0000

    modifie 0-safe_print_list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,10 +3,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
             count += 1
-    except IndexError:
+    except Exception:
         pass
-    finally:
-        print()
+    print()
     return count
