602a6be5ad64e9586176f39e7ca304ebe69cf3ee
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-05-16 06:56:48 +0000

    add function matrix square

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        K = []
+        for column in row:
+            a.append(column * column)
+        new_matrix.append(K)
+    return new_matrix
