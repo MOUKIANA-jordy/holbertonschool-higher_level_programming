f84918231dd541c12b43d3f12da2d1a457f813a3
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-05-16 12:48:39 +0000

    all function

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-def common_elements(set_1, set_2):
-    cache = []
-    c_set = []
-    for _set in [set_1, set_2]:
-        for el in _set:
-            if el in cache:
-                c_set.append(el)
-            else:
-                cache.append(el)
-    return c_set
+def uniq_add(my_list=[]):
+    new_list = set(my_list)
+    total_sum = sum(new_list)
+    return total_sum
