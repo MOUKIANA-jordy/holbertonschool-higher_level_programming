f1dc3de5ac6ab4914aca24562952ec7c32779454
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-05-17 20:30:17 +0000

    all function

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,6 +5,9 @@
 class Square:
     """Square class"""
 
+    def area(self):
+        return (self.__size**2)
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
