c40062d7a12fdc7009e7d3de3804544575bb0d3e
root <MOUKIANA-jordy>
2024-05-27 13:33:24 +0000

    modif function read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,11 @@
-#!i/usr/bin/python3
+#!/usr/bin/python3
 """
     function that reads a text file (UTF8) and prints it to stdout
 """
 
 
 def read_file(filename=""):
-     """
+    """
         Reads a text file (UTF8) and prints it to stdout.
         Arguments:
             filename (str): File to open.
