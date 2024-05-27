f17ce923c50efa65530a46cf7c60d779769dd3bb
root <MOUKIANA-jordy>
2024-05-27 10:25:15 +0000

    add function student

---
+++ python-input_output/9-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+class Student:
+    """
+    Defines a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+
+        """
+        Initializes a Student instance with first_name,
+         last_name, and age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of
+        a Student instance.
+        """
+        json_representation = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
+        return json_representation
