f56f630959574bd958e0fd6bd90d635c15f80af9
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-05-28 14:36:55 +0000

    modif function item

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,8 +3,10 @@
 This script adds all arguments to a Python list and saves them to a file.
 """
 
+
 import sys
 import os.path
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__(
