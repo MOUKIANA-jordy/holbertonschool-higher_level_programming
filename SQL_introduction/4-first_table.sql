b3c668a1f60cdf31ef5b02ef46737d2beda72ded
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-06-19 12:43:45 +0200

    add first table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- the script  creates a table called first_table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT
+);
+
