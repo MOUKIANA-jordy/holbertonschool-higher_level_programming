ce5c1ad1b8ad83e79c5027877f466d4190f819ec
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-06-20 14:04:58 +0200

    add privileges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
--- MY privileges
--- SHOW privs for user1
-SHOW GRANTS FOR user_0d_1@localhost;
--- show privs for user2
-SHOW GRANTS FOR user_0d_2@localhost;
+-- My privileges
+-- show privs for users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
