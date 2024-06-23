e3d4e263d39d55dae0590860ee12b3b66e998048
MOUKIANA-jordy <jordymoukiana@gmail.com>
2024-06-23 16:51:35 +0200

    modif model state

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
-"""List all states"""
-from sys import argv
+"""
+    Script that changes the name of a State object from the database.
+"""
+import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
+
 
 if __name__ == "__main__":
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    states = session.query(State).filter(State.id == 2).all()
-    if states:
-        states[0].name = "New Mexico"
-    session.commit()
-    session.close()
+
+    session = Session(engine)
+    q = session.query(State).where(State.id.ilike(2))
+    state = q.one()
+    if state:
+        state.name = "New Mexico"
+        session.commit()
